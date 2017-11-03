c714792868a516c0e9d4cf25aa357b5f4d9ac9c8
hjyai94 <hjyai94@gmail.com>
2017-11-03 22:19:02 +0800

    Fibonacci sequence
    求Fibonacci sequence中的偶数和

--- prob2.m
+++ prob2.m
@@ -1,6 +1,17 @@
-function [ y ] = prob2(x)
-%By considering the terms in the Fibonacci sequence whose 
-%value do not exceed four million ,find the sum of the even-valued terms
-
-
+%计算Fibonacci数列中偶数和
+clear;
+clc;
+format compact
+f(1) = 1;
+f(2) = 2;
+sum = 2;
+for i = 3:10000000
+    f(i) = f(i-1) +f(i-2);
+    if(f(i)) <= 6000000
+        if(rem(f(i),2)==0)
+            sum = sum  + f(i);
+    end
+    end
 end
+disp('sum = ');
+sum
