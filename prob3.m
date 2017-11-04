4443606273ed8af223768d53f54f7223842bfd54
hjyai94 <hjyai94@gmail.com>
2017-11-04 11:57:13 +0800

    problem 3
    求600851475143的最大约数

--- prob3.m
+++ prob3.m
@@ -1,7 +1,13 @@
-function prime_factor =prob3 [x]
-%求 600851475143 的最大公约数
-x =  600851475143 ;
-for i = 1: 600851475143 
-    if(rem(600851475143,i)==0)
-        temp = i;
-        
+function prime_factor =prob3 (x)
+%求 600851475143 的最大约数
+temp = 1;
+for i = 2: x-1
+    if(rem(x,i)==0 && rem(i,2)~=0)
+        temp = i*temp;
+        if(temp == x)
+            prime_factor = i;
+            prime_factor
+        end
+    end
+end
+end
